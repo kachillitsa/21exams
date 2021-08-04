d94cd51be295351a6d95fc3ef1d517d6f6a0ed19
Birgit Jebediah <bjebedia@oa-n3.msk.21-school.ru>
2021-08-04 18:00:18 +0300

    1

--- rank06/new_try.c
+++ rank06/new_try.c
@@ -8,7 +8,7 @@
 
 // 3 инта, фд_сета, чара
 int		sock_id[42*4242], next_id = 0, max_sock = 0;
-fd_set	sfd_act, afd_read, sfd_write;
+fd_set	sfd_act, sfd_read, sfd_write;
 char	buf_str[42*4242], buf_read[42*4242], buf_write[42*4242+42];
 
 void fatal_error() {
@@ -30,6 +30,22 @@ int		main(int ac, char **av) {
 	FD_SET(sockfd, &sfd_act);
 	struct sockaddr_in servaddr;
 	socklen_t len = sizeof(servaddr);
-	
-
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
+	servaddr.sin_port = htons(atoi(av[1]));
+	if ((bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) < 0)
+		fatal_error();
+	if (listen(sockfd, 128) < 0)
+		fatal_error();
+	while (1) {
+		sfd_read = sfd_write = sfd_act;
+		if (select(max_sock + 1, &sfd_read, &sfd_write, NULL, NULL) < 0)
+			continue;
+		for (int sel_sock = 0; sel_sock <= max_sock; sel_sock++) {
+			if (FD_ISSET(sel_sock, &sfd_read) && sel_sock == sockfd) {
+				
+			}
+		}
+	}
+	return (0);
 }
